6906db528a5668b4b788bf967471d00597083ec4
bnagain <bnagain@hanmail.net>
2020-06-14 22:30:50 +0900

    Updates

---
+++ precache-manifest.069ecbf71d1ebf24aa871982f87d048e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6684bee4f0f3850ae7b631cc1963ca27",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "f4934b06a1f13820841c",
+    "url": "/react-movie/static/css/main.1ecf8787.chunk.css"
+  },
+  {
+    "revision": "c3e4ea043026d44d3369",
+    "url": "/react-movie/static/js/2.f0115c8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie/static/js/2.f0115c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4934b06a1f13820841c",
+    "url": "/react-movie/static/js/main.574f5870.chunk.js"
+  },
+  {
+    "revision": "1964db4a2d4e6e18fd63",
+    "url": "/react-movie/static/js/runtime-main.14efe68a.js"
+  }
+]);
